acc35403b0f6b3e73e9f11ba63faafab8d21e82f
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-12-19 14:53:00 +0100

    ca me saoule

--- server/src/modules/album/AlbumActions.ts
+++ server/src/modules/album/AlbumActions.ts
@@ -3,4 +3,23 @@ import { StatusCodes } from "http-status-codes";
 import AlbumRepository from "./AlbumRepository";
 
 const add: RequestHandler = async (req, res, next) => {
+  try {
+    if (!req.body.title || typeof req.body.title !== "string") {
+      res.sendStatus(StatusCodes.BAD_REQUEST);
+    }
+
+    // ToDo : Penser à remplacer la valeur 1 par l'ID de l'utilisateur connecté
+    const newAlbum = {
+      title: req.body.title,
+      userId: 1,
+    };
+
+    const newAlbumId = AlbumRepository.insert(newAlbum);
+
+    res.status(StatusCodes.CREATED).json({ newAlbumId });
+  } catch (err) {
+    next(err);
+  }
 };
+
+export default { add };
