db33ad8d8c057302bb480535ada8108ab54e6ccd
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-12-19 14:51:47 +0100

    Add route to post album

---
+++ server/src/modules/album/AlbumActions.ts
@@ -0,0 +1,6 @@
+import type { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+import AlbumRepository from "./AlbumRepository";
+
+const add: RequestHandler = async (req, res, next) => {
+};
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import AlbumActions from "./modules/album/AlbumActions";
 
 const router = express.Router();
 
@@ -6,13 +7,5 @@ const router = express.Router();
 // Define Your API Routes Here
 /* ************************************************************************* */
 
-// Define item-related routes
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
-/* ************************************************************************* */
-
+router.post("/api/albums", AlbumActions.add);
 export default router;
